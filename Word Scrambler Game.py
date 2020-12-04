ba594ebfef6f86aa448daa011d61d20b0d8a0aef
Dhruv2550 <63900923+Dhruv2550@users.noreply.github.com>
2020-12-04 19:38:23 +0400

    Create Word Scrambler Game.py

---
+++ Word Scrambler Game.py
@@ -0,0 +1,121 @@
+print('Welcome to the Physics Quiz, ENJOY!!!')
+
+unscramble1 = input('Unscramble the word (olvaget)\n').lower().strip()
+if unscramble1 == 'voltage':
+    print(f'You got it correct!!!, onto question number 2')
+else:
+    print(f'You got it wrong, 2nd attempt')
+    unscramble1 = input('Unscramble the word (olvaget)\n').lower().strip()
+    if unscramble1 == 'voltage':
+        print(f'You got it correct!!!, onto question number 2')
+    else:
+        print(f'Sorry 2 incorect attempts, restart ')
+        exit()
+
+unscramble2 = input('Unscramble the word (eraltingnat rrcuten)\n').lower().strip()
+if unscramble2 == 'alternating current':
+    print(f'You got it correct!!!, onto question number 3')
+else:
+    print(f'You got it wrong, 2nd attempt')
+    unscramble2 = input('Unscramble the word (eraltingnat rrcuten)\n').lower().strip()
+    if unscramble2 == 'alternating current':
+        print(f'You got it correct!!!, onto question number 3')
+    else:
+        print(f'Sorry 2 incorect attempts, restart ')
+        exit()
+
+unscramble3 = input('Unscramble the word (maretme)\n').lower().strip()
+if unscramble3 == 'ammeter':
+    print(f'You got it correct!!!, onto question number 4')
+else:
+    print(f'You got it wrong, 2nd attempt')
+    unscramble3 = input('Unscramble the word (maretme)\n').lower().strip()
+    if unscramble3 == 'ammeter':
+        print(f'You got it correct!!!, onto question number 4')
+    else:
+        print(f'Sorry 2 incorect attempts, restart ')
+        exit()
+
+unscramble4 = input('Unscramble the word (repame)\n').lower().strip()
+if unscramble4 == 'ampere':
+    print(f'You got it correct!!!, onto question number 5')
+else:
+    print(f'You got it wrong, 2nd attempt')
+    unscramble4 = input('Unscramble the word (repame)\n').lower().strip()
+    if unscramble4 == 'ampere':
+        print(f'You got it correct!!!, onto question number 5')
+    else:
+        print(f'Sorry 2 incorect attempts, restart ')
+        exit()
+
+unscramble5 = input('Unscramble the word (leceortn)\n').lower().strip()
+if unscramble5 == 'electron':
+    print(f'You got it correct!!!, onto question number 6')
+else:
+    print(f'You got it wrong, 2nd attempt')
+    unscramble5 = input('Unscramble the word (leceortn)\n').lower().strip()
+    if unscramble5 == 'electron':
+        print(f'You got it correct!!!, onto question number 6')
+    else:
+        print(f'Sorry 2 incorect attempts, restart ')
+        exit()
+
+unscramble6 = input('Unscramble the word (aprlalel)\n').lower().strip()
+if unscramble6 == 'parallel':
+    print(f'You got it correct!!!, onto question number 7')
+else:
+    print(f'You got it wrong, 2nd attempt')
+    unscramble6 = input('Unscramble the word (aprlalel)\n').lower().strip()
+    if unscramble6 == 'parallel':
+        print(f'You got it correct!!!, onto question number 7')
+    else:
+        print(f'Sorry 2 incorect attempts, restart ')
+        exit()
+
+unscramble7 = input('Unscramble the word (tchiws)\n').lower().strip()
+if unscramble7 == 'switch':
+    print(f'You got it correct!!!, onto question number 8')
+else:
+    print(f'You got it wrong, 2nd attempt')
+    unscramble7 = input('Unscramble the word (tchiws)\n').lower().strip()
+    if unscramble7 == 'switch':
+        print(f'You got it correct!!!, onto question number 8')
+    else:
+        print(f'Sorry 2 incorect attempts, restart ')
+        exit()
+
+unscramble8 = input('Unscramble the word (temtolvre)\n').lower().strip()
+if unscramble8 == 'voltmeter':
+    print(f'You got it correct!!!, onto question number 9')
+else:
+    print(f'You got it wrong, 2nd attempt')
+    unscramble8 = input('Unscramble the word (temtolvre)\n').lower().strip()
+    if unscramble8 == 'voltmeter':
+        print(f'You got it correct!!!, onto question number 9')
+    else:
+        print(f'Sorry 2 incorect attempts, restart ')
+        exit()
+
+unscramble9 = input('Unscramble the word (rotsiser)\n').lower().strip()
+if unscramble9 == 'resistor':
+    print(f'You got it correct!!!, onto question number 10')
+else:
+    print(f'You got it wrong, 2nd attempt')
+    unscramble9 = input('Unscramble the word (rotsiser)\n').lower().strip()
+    if unscramble9 == 'resistor':
+        print(f'You got it correct!!!, onto question number 10')
+    else:
+        print(f'Sorry 2 incorect attempts, restart ')
+        exit()
+
+unscramble10 = input('Unscramble the word (pwero)\n').lower().strip()
+if unscramble10 == 'power':
+    print(f'Congratulations!!! You Scored 10/10')
+else:
+    print(f'You got it wrong, 2nd attempt')
+    unscramble10 = input('Unscramble the word (pwero)\n').lower().strip()
+    if unscramble10 == 'power':
+        print(f'Congratulations!!! You Scored 10/10')
+    else:
+        print(f'Sorry 2 incorect attempts, restart ')
+        exit()
